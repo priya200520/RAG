24c37f7e2a8833164ef88cd6d9c78daf334ee61d
priya <ramgariyapriya@gmail.com>
2026-07-29 17:17:36 +0530

    update

--- document_loaders.py
+++ document_loaders.py
@@ -1,5 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
 loader = PyPDFLoader("data/RAG_Practice_Sample.pdf")
-
+#pypdf_loader.py
 documents = loader.load()
-print(type(documents))
+documents = loader.lazy_load()
+
+#print(len(documents))
+#print(documents[0])
+# TextLoaader
+print(documents[0].page_content)
